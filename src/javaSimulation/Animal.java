684651c08199d60c4e7673e1f9d19cdbf3f3cbc0
shoe_gazer <omar.boukmakh@efrei.net>
2025-12-27 16:00:47 +0100

    updated movement pattern & deleted Move() override

--- src/javaSimulation/Animal.java
+++ src/javaSimulation/Animal.java
@@ -1,6 +1,10 @@
 package javaSimulation;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public abstract class Animal {
+	
+	private static final Random RANDOM = new Random();
 	
 	private final CellColor color;
 	private Position position;
@@ -22,7 +26,15 @@ public abstract class Animal {
 		this.position = pos;
 	}
 	
-	public abstract void Move(int gridSize);
+	protected int randomStep() {
+		return RANDOM.nextInt(3) - 1;     
+	}
+	
+	public void Move(int gridSize) {
+		int x = position.getX() + randomStep();
+        int y = position.getY() + randomStep();
+        position = new Position(x, y).wrap(gridSize);
+	}
 
 }
 
@@ -32,29 +44,12 @@ class RedAnimal extends Animal{
 		super(CellColor.RED, position);
 	}
 	
-	public void Move(int gridSize) {
-		
-		int X = getPosition().getX()+1;
-		int Y = getPosition().getY()-2;
-	    
-		Position newPos = new Position(X, Y).wrap(gridSize);
-	    setPosition(newPos);
-	}
 }
 
 class GreenAnimal extends Animal{
 	
 	public GreenAnimal(Position position) {
 		super(CellColor.GREEN, position);
-	}
-	
-	public void Move(int gridSize) {
-		
-		int X = getPosition().getX()-1;
-		int Y = getPosition().getY()+1;
-	    
-		Position newPos = new Position(X, Y).wrap(gridSize);
-	    setPosition(newPos);
 	}
 	
 }
@@ -65,15 +60,6 @@ class YellowAnimal extends Animal{
 		super(CellColor.YELLOW, position);
 	}
 	
-	public void Move(int gridSize) {
-		
-		int X = getPosition().getX()+1;
-		int Y = getPosition().getY()+1;
-	    
-		Position newPos = new Position(X, Y).wrap(gridSize);
-	    setPosition(newPos);
-	}
-
 }
 
 
